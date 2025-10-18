5d01055d5d0b11dbb66a37489dc740494d902f86
T-B404 <tasneembharmal712@gmail.com>
2025-10-18 17:09:55 +0530

    MongoDB Integration + Lombok

---
+++ src/main/java/com/edigest/journalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,10 @@
+package com.edigest.journalApp.repository;
+
+import com.edigest.journalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+// controller ---> service ---> repository
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+
+}
